4509fab84137f4e9a5578bde5b4599cd497ec8d6
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-13 12:30:24 +0800

    [Jiachen] feat-4: add structure for detail page

---
+++ client/scripts/details.js
@@ -0,0 +1,16 @@
+/* const BASIC_URL = "http://127.0.0.1:8888/client";
+const pathUrl = "/v2/movie/top250";
+
+ajax({
+	url: BASIC_URL + pathUrl,
+	method: "GET",
+	data: {
+		apikey: "0df993c66c0c636e29ecbb5344252a4a",
+	},
+	success: function(response) {
+		console.log(response);
+	}, // 请求成功后调用此方法
+	fail: function(error) {
+		console.log("request fail!");
+	}, // 请求失败或出错后调用此方法
+}); */
