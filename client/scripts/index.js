249a36f4e52a31b369b6432734bdcf9c1cbae6b2
RicoTech666 <zhoujiachenbe@gmail.com>
2020-01-13 23:20:21 +0800

    [Jiachen] refactor-1: refactor the function to get all genres

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -3,40 +3,26 @@ let movieId = "26942674";
 
 function loadAllData() {
 	ajax({
-		url:
-			BASIC_URL +
-			"/v2/movie/top250" +
-			"?apikey=0df993c66c0c636e29ecbb5344252a4a" +
-			"&start=0&count=250",
+		url: BASIC_URL + "/v2/movie/top250" + "?apikey=0df993c66c0c636e29ecbb5344252a4a" + "&start=0&count=250",
 		method: "GET",
 		success: function(res) {
 			getMoviesGenres(res);
 			//addAllData(res);
-		}
+		},
 	});
 }
 
 function getMoviesGenres(movies) {
-	let genres = [];
-	let movieGenres = [];
-	movies.subjects.forEach(item => {
-		genres.push(item.genres);
-	});
-	genres.forEach(item => {
-		item.forEach(genre => {
-			if (movieGenres.indexOf(genre) === -1) {
-				movieGenres.push(genre);
-			}
-		});
-	});
-	addMoviesGenres(movieGenres);
+	let movieGenres = new Set(movies.subjects.map(movie => movie.genres).flat());
+
+	addMoviesGenres([...movieGenres]);
 }
 
 function addMoviesGenres(genres) {
 	let genersList = document.getElementsByClassName("genres-name")[0];
 	genres.forEach(genre => {
 		genersList.innerHTML += `<li class="movie-genres">${genre}</li>`;
-	})
+	});
 }
 
 loadAllData();
